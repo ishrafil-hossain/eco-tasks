fbe4a28f5dfc9bb232bedd802b36a2b8501c2e08
ishrafil-hossain <ishrafil2233@gmail.ocm>
2024-12-18 17:22:07 +0600

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    // You can add more paths here based on your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
